456e15bb77c96e201c91d95034b7bf49ee5a64e9
Prasanna Ram Venkatachalam <pras@simpllyf.io>
2023-07-15 09:45:10 -0700

    modify cloudflare example to actually use gb

--- examples/cloudflare/src/lib.rs
+++ examples/cloudflare/src/lib.rs
@@ -1,3 +1,10 @@
+use chrono::Utc;
+use growthbook_sdk_rust::growthbook::GrowthBook;
+use growthbook_sdk_rust::model::ContextBuilder;
+use growthbook_sdk_rust::model::Experiment;
+use growthbook_sdk_rust::model::ExperimentBuilder;
+use growthbook_sdk_rust::model::ExperimentResult;
+use growthbook_sdk_rust::model::TrackingCallback;
 use growthbook_sdk_rust::repository::FeatureRefreshCallback;
 use growthbook_sdk_rust::repository::FeatureRepositoryBuilder;
 use serde_json::json;
@@ -30,12 +37,63 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                 .client_key(Some("java_NsrWldWd5bxQJZftGsWKl7R2yD2LtAK8C8EUYh9L8".to_string()))
                 .build()
                 .unwrap();
-            repo.get_features().await;
+            let features = repo.get_features().await;
 
-            let json = json!({
-                "features": repo.get_features().await,
+            let user_attributes = json!({
+                "id"                 :"user-employee-123456789",
+                "loggedIn"            :true,
+                "employee"            :true,
+                "country"           :"france",
+                "dietaryRestrictions": ["gluten-free"],
             });
-            return Response::from_json(&json);
+
+            // This will get called when the font_colour experiment below is evaluated
+            let tracking_callback = TrackingCallback(Box::new(move |experiment: Experiment, result: ExperimentResult| {
+                println!(
+                    "Experiment Viewed: {:?} - Variation index: {:?} - Value: {:?}",
+                    experiment.key, result.variation_id, result.value
+                )
+            }));
+            let gb = GrowthBook {
+                context: ContextBuilder::default()
+                    .attributes(user_attributes)
+                    .features(features.clone())
+                    .build()
+                    .unwrap(),
+                tracking_callback: Some(tracking_callback),
+            };
+            let banner_text = gb.get_feature_value_as_str("banner_text", "???");
+            let use_dark_mode = gb.is_on("dark_mode");
+            let default_meal_type = json!({
+                "MealType": "standard",
+               "Dessert": "Apple Pie",
+            });
+            let meal_type = gb.get_feature_value("meal_overrides_gluten_free", &default_meal_type);
+
+            let experiment = ExperimentBuilder::default()
+                .key("font_colour".to_string())
+                .variations(vec![
+                    json!("red"),
+                    json!("orange"),
+                    json!("yellow"),
+                    json!("green"),
+                    json!("blue"),
+                    json!("purple"),
+                ])
+                .build()
+                .unwrap();
+
+            let result = gb.run(&experiment);
+            let username_colour = result.value.as_str().unwrap();
+            let response = json!({
+                "banner_text": banner_text,
+                "use_dark_mode": use_dark_mode,
+                "meal_type": meal_type,
+                "username_colour": username_colour,
+                "time": Utc::now().to_rfc3339(),
+            });
+
+            return Response::from_json(&response);
         })
         .run(req, env)
         .await
